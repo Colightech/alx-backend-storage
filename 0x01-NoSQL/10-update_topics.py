8c1bb78aeb961979b4de27d97737fcf47e2a4739
Colightech <saintchris4@gmail.com>
2024-05-14 09:17:40 -0700

    task 11-schools_by_topic.py completed

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,7 +5,8 @@
 
 def update_topics(mongo_collection, name, topics):
     '''Function that changes all topics of a collection's document based on
-        the name.
+        the name. the function updated the topics where the attribute name is
+        specify
     '''
     mongo_collection.update_many(
         {'name': name},
